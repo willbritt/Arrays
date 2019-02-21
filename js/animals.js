ccf57d835b45deae811444396c0ab4d097102e1e
ehstowe <47224648+ehstowe@users.noreply.github.com>
2019-02-20 19:55:20 -0500

    Update animals.js

--- js/animals.js
+++ js/animals.js
@@ -5,18 +5,11 @@ var createList = function()
  var names = [ "lion", "zebra", "giraffe", "hippo"];
  
  document.getElementById("animal1").innerHTML = "1. "+names[0];
- document.getElementById("animal2").innerHTML = names[1];
- document.getElementById("animal3").innerHTML = names[2];
- document.getElementById("animal4").innerHTML = names[3];
+ document.getElementById("animal2").innerHTML = "2. "+names[1];
+ document.getElementById("animal3").innerHTML = "3. "+names[2];
+ document.getElementById("animal4").innerHTML = "4. "+names[3];
  
  
- 
- 
- /*var r=document.createElement("r");
- p.innerText="3."+names[2];
- 
- var s=document.createElement("s");
- p.innerText="4."+names[3];*/
  
  
 
